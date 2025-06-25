869b56be994a23a7c69f675be26233bc1e5e7d17
theProton <parthivmenon.dev@gmail.com>
2025-06-25 22:11:54 +0530

    Add privacy policy page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from debrief import SiteToDebrief
 from utils import setup_env
 from urllib.parse import unquote 
@@ -21,6 +23,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 async def index():
     return {"message": "API is functional"}
@@ -31,6 +35,10 @@ async def get_site_data(link: str):
     site_to_debrief: SiteToDebrief = SiteToDebrief(url=url)
     site_data: dict = site_to_debrief.get_site_data()
     return site_data
+
+@app.get("/privacy", response_class=HTMLResponse)
+async def privacy(request: Request):
+    return templates.TemplateResponse("privacy/index.html", { "request": request })
 
 if __name__ == "__main__":
     import uvicorn
